8cd7f7dae7bb433257394e2b784a851672587128
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-10-12 15:50:50 +0900

    Singletonの例を追加

---
+++ src/Singleton/Exercise/Main.java
@@ -0,0 +1,15 @@
+package Singleton.Exercise;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Start.");
+    Singleton obj1 = Singleton.getInstance();
+    Singleton obj2 = Singleton.getInstance();
+    if (obj1 == obj2) {
+      System.out.println("Same");
+    } else {
+      System.out.println("Different");
+    }
+    System.out.println("End.");
+  }
+}
