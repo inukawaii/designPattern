81601093eaa36c560cdba47a9d7b292606d6f0cf
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-10-22 23:40:02 +0900

    解答追加

--- src/Builder/Exercise/q2/TextBuilder.java
+++ src/Builder/Exercise/q2/TextBuilder.java
@@ -3,22 +3,22 @@ package Builder.Exercise.q2;
 public class TextBuilder extends Builder {
   private StringBuffer buffer = new StringBuffer();
 
-  public void makeTitle(String title) {
+  public void buildTitle(String title) {
     buffer.append("=================\n");
     buffer.append("[" + title + "]\n");
     buffer.append("\n");
   }
-  public void makeString(String str) {
+  public void buildString(String str) {
     buffer.append('■' + str + "\n");
     buffer.append("\n");
   }
-  public void makeItems(String[] items) {
+  public void buildItems(String[] items) {
     for (int i = 0; i < items.length; i++) {
       buffer.append("  ." + items[i] +"\n");
     }
     buffer.append("\n");
   }
-  public void close() {
+  public void buildDone() {
     buffer.append("=================\n");
   }
   public String getResult() {
