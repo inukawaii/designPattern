dde414b808b968029a0b1a9efc543035efae1f3c
mayumi aka inukawaii <inukawaii999@gmail.com>
2021-11-01 12:39:54 +0900

    追加で例に具体的な工場を追加

---
+++ src/AbstractFactory/Example/tablefactory/TableFactory.java
@@ -0,0 +1,15 @@
+package AbstractFactory.Example.tablefactory;
+
+import AbstractFactory.Example.factory.*;
+
+public class TableFactory extends Factory {
+  public Link createLink(String caption, String url) {
+    return new TableLink(caption, url);
+  }
+  public Tray createTray(String caption) {
+    return new TableTray(caption);
+  }
+  public Page createPage(String title, String author) {
+    return new TablePage(title, author);
+  }
+}
